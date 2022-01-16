8adfbc26104a53344c6e416b355651dc5188196f
Alvin Ashcraft <alashcraft@gmail.com>
2022-01-16 13:29:42 -0500

    Add two more samples to chapter 1
    Add samples to chapter 1 for parameterized threading calls and pausing threads.

---
+++ chapter01/BackgroundPingConsoleApp_2_param/BackgroundPingConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hello, World!");
+
+var bgThread = new Thread((object? data) =>
+{
+    if (data is null) return;
+
+    int counter = 0;
+    var result = int.TryParse(data.ToString(), out int maxCount);
+    if (!result) return;
+
+    while (counter < maxCount)
+    {
+        bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+        Thread.Sleep(100);
+        counter++;
+    }
+});
+
+bgThread.IsBackground = true;
+bgThread.Start(12);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Main thread working...");
+    Task.Delay(500);
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
---
+++ chapter01/BackgroundPingConsoleApp_3_pause/BackgroundPingConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+
+var bgThread = new Thread((object? data) =>
+{
+    if (data is null) return;
+
+    int counter = 0;
+    var result = int.TryParse(data.ToString(), out int maxCount);
+    if (!result) return;
+
+    while (counter < maxCount)
+    {
+        bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+        Thread.Sleep(10);
+        counter++;
+    }
+});
+
+bgThread.Start(12);
+
+for (int i = 0; i < 12; i++)
+{
+    Console.WriteLine("Main thread working...");
+    Thread.Sleep(100);
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
