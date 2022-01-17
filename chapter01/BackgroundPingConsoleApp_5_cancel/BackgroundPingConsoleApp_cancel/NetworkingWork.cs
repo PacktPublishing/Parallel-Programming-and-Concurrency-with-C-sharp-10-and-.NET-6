13d729ea927e247f33418716d9e59b9e13887199
Alvin Ashcraft <alashcraft@gmail.com>
2022-01-17 14:58:37 -0500

    Add new method to thread cancellation example
    Added a new method to the thread cancellation sample to cancel via a delegate.

--- chapter01/BackgroundPingConsoleApp_5_cancel/BackgroundPingConsoleApp_cancel/NetworkingWork.cs
+++ chapter01/BackgroundPingConsoleApp_5_cancel/BackgroundPingConsoleApp_cancel/NetworkingWork.cs
@@ -5,8 +5,23 @@
         public void CheckNetworkStatus(object data)
         {
             var cancelToken = (CancellationToken)data;
-
             while (!cancelToken.IsCancellationRequested)
+            {
+                bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+            }
+        }
+
+        public void CheckNetworkStatus2(object data)
+        {
+            bool finish = false;
+            var cancelToken = (CancellationToken)data;
+            cancelToken.Register(() => {
+                // Clean up and end pending work
+                finish = true;
+            });
+
+            while (!finish)
             {
                 bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
                 Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
