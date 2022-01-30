a8482bfa3407d0d7bbf1cf0d1bcdc281a4aef9c9
Alvin Ashcraft <alashcraft@gmail.com>
2022-01-30 15:43:50 -0500

    Chapter 2 - Add threadpool sample app

---
+++ chapter02/BackgroundPingConsoleApp_pool/BackgroundPingConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+
+ThreadPool.QueueUserWorkItem((o) =>
+{
+    for (int i = 0; i < 20; i++)
+    {
+        bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+        Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+        Thread.Sleep(100);
+    }
+});
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Main thread working...");
+    Task.Delay(500);
+}
+
+Console.WriteLine("Done");
+Console.ReadKey();
