dbb97965c0a50d88c0d23194a72e80d9918edf39
Alvin Ashcraft <alashcraft@gmail.com>
2022-02-06 12:39:05 -0500

    Add async example to chapter 2
    Adding an example using async/await in a console application for chapter 2.

---
+++ chapter02/AsyncConsoleExample/NetworkHelper.cs
@@ -0,0 +1,28 @@
+﻿namespace AsyncConsoleExample
+{
+    internal class NetworkHelper
+    {
+        internal async Task CheckNetworkStatusAsync()
+        {
+            Task t = NetworkCheckInternalAsync();
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine("Top level method working...");
+                await Task.Delay(500);
+            }
+
+            await t;
+        }
+
+        private async Task NetworkCheckInternalAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                bool isNetworkUp = System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable();
+                Console.WriteLine($"Is network available? Answer: {isNetworkUp}");
+                await Task.Delay(100);
+            }
+        }
+    }
+}
---
+++ chapter02/AsyncConsoleExample/Program.cs
@@ -0,0 +1,9 @@
+﻿using AsyncConsoleExample;
+
+Console.WriteLine("Hello, async!");
+
+var networkHelper = new NetworkHelper();
+await networkHelper.CheckNetworkStatusAsync();
+
+Console.WriteLine("Main method complete.");
+Console.ReadKey();
