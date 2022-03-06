31e62f06f4e5f4cd1f7b072839b534ecd0868e0d
Alvin Ashcraft <alashcraft@gmail.com>
2022-03-06 15:57:47 -0500

    Add TaskSample to chapter 5 AsyncSamples

--- chapter05/AsyncSamples/Program.cs
+++ chapter05/AsyncSamples/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AsyncSamples;
+
+Console.WriteLine("Start processing...");
+var taskSample = new TaskSample();
+await taskSample.DoThingsAsync();
+Console.WriteLine("Continue processing...");
+await taskSample.DoingThingsWrongAsync();
+Console.WriteLine("Continue processing...");
+await taskSample.DoBlockingThingsAsync();
+Console.WriteLine("Done processing...");
---
+++ chapter05/AsyncSamples/TaskSample.cs
@@ -0,0 +1,50 @@
+﻿namespace AsyncSamples
+{
+    public class TaskSample
+    {
+        public async Task DoThingsAsync()
+        {
+            Console.WriteLine($"Doing things in {nameof(DoThingsAsync)}");
+            await DoFirstThingAsync();
+            await DoSecondThingAsync();
+            Console.WriteLine($"Did things in {nameof(DoThingsAsync)}");
+        }
+
+        public async Task DoingThingsWrongAsync()
+        {
+            Console.WriteLine($"Doing things in {nameof(DoingThingsWrongAsync)}");
+            DoFirstThingAsync();
+            await DoSecondThingAsync();
+            Console.WriteLine($"Did things in {nameof(DoingThingsWrongAsync)}");
+        }
+
+        public async Task DoBlockingThingsAsync()
+        {
+            Console.WriteLine($"Doing things in {nameof(DoBlockingThingsAsync)}");
+            DoFirstThingAsync().Wait();
+            await DoSecondThingAsync();
+            Console.WriteLine($"Did things in {nameof(DoBlockingThingsAsync)}");
+        }
+
+        private async Task DoFirstThingAsync()
+        {
+            Console.WriteLine($"Doing something in {nameof(DoFirstThingAsync)}");
+            await DoAnotherThingAsync();
+            Console.WriteLine($"Did something in {nameof(DoFirstThingAsync)}");
+        }
+
+        private async Task DoSecondThingAsync()
+        {
+            Console.WriteLine($"Doing something in {nameof(DoSecondThingAsync)}");
+            await Task.Delay(500);
+            Console.WriteLine($"Did something in {nameof(DoSecondThingAsync)}");
+        }
+
+        private async Task DoAnotherThingAsync()
+        {
+            Console.WriteLine($"Doing something in {nameof(DoAnotherThingAsync)}");
+            await Task.Delay(1500);
+            Console.WriteLine($"Did something in {nameof(DoAnotherThingAsync)}");
+        }
+    }
+}
