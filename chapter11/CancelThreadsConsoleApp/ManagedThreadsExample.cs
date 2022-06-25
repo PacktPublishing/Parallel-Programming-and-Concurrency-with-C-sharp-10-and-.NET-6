4f4ec5264188ce0c2e1036231c1e0479c2d28949
Alvin Ashcraft <alashcraft@gmail.com>
2022-06-25 13:38:54 -0400

    Add first chapter 11 examples
    Add cancellation examples for managed threads, parallel loops, and PLINQ.

---
+++ chapter11/CancelThreadsConsoleApp/ManagedThreadsExample.cs
@@ -0,0 +1,66 @@
+﻿namespace CancelThreadsConsoleApp
+{
+    public class ManagedThreadsExample
+    {
+        public static void ProcessText(object? cancelToken)
+        {
+            var token = cancelToken as CancellationToken?;
+            string text = "";
+
+            for (int x = 0; x < 75000; x++)
+            {
+                if (token != null && token.Value.IsCancellationRequested)
+                {
+                    Console.WriteLine($"Cancellation request received. String value: {text}");
+                    break;
+                }
+                text += x + " ";
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void ProcessTextParallel(object? cancelToken)
+        {
+            var token = cancelToken as CancellationToken?;
+            if (token == null) return;
+            string text = "";
+            ParallelOptions options = new()
+            {
+                CancellationToken = token.Value,
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+            try
+            {
+                Parallel.For(0, 75000, options, (x) =>
+                {
+                    text += x + " ";
+                    Thread.Sleep(500);
+                });
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine($"Text value: {text}. {Environment.NewLine} Exception encountered: {e.Message}");
+            }
+        }
+
+        public static void ProcessNumsPlinq(object? cancelToken)
+        {
+            int[] input = Enumerable.Range(1, 25000000).ToArray();
+            var token = cancelToken as CancellationToken?;
+            if (token == null) return;
+            int[]? result = null;
+            try
+            {
+                result =
+                    (from value in input.AsParallel().WithCancellation(token.Value)
+                     where value % 7 == 0
+                     orderby value
+                     select value).ToArray();
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine($"Exception encountered: {e.Message}");
+            }
+        }
+    }
+}
---
+++ chapter11/CancelThreadsConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CancelThreadsConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CancelThread();
+            //CancelParallelFor();
+            CancelPlinq();
+            Console.ReadKey();
+        }
+
+        private static void CancelThread()
+        {
+            using CancellationTokenSource tokenSource = new();
+            Console.WriteLine("Starting operation.");
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ManagedThreadsExample.ProcessText), tokenSource.Token);
+            Thread.Sleep(5000);
+            Console.WriteLine("Requesting cancellation.");
+            tokenSource.Cancel();
+            Console.WriteLine("Cancellation requested.");
+        }
+
+        private static void CancelParallelFor()
+        {
+            using CancellationTokenSource tokenSource = new();
+            Console.WriteLine("Press a key to start, then press 'x' to send cancellation.");
+            Console.ReadKey();
+            Task.Run(() =>
+            {
+                if (Console.ReadKey().KeyChar == 'x')
+                    tokenSource.Cancel();
+                Console.WriteLine();
+                Console.WriteLine("press a key");
+            });
+            ManagedThreadsExample.ProcessTextParallel(tokenSource.Token);
+        }
+
+        private static void CancelPlinq()
+        {
+            using CancellationTokenSource tokenSource = new();
+            Console.WriteLine("Press a key to start.");
+            Console.ReadKey();
+            Task.Run(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Requesting cancel.");
+                tokenSource.Cancel();
+                Console.WriteLine("Cancel requested.");
+            });
+            ManagedThreadsExample.ProcessTextPlinq(tokenSource.Token);
+        }
+    }
+}
